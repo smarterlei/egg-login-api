44c1bd21ea45ecc405d68881ce05740d83aa7f01
wl301 <1015115060@qq.com>
2022-06-14 09:14:59 +0800

    新增文件上传接口，返回成功后的文件路径

--- app/service/upload.js
+++ app/service/upload.js
@@ -2,7 +2,7 @@ module.exports = app => {
     class Service extends app.Service {
           async uploadindex(request){
               let url = request.url
-            const result=  await this.app.mysql.insert('uploadfilePath', { url:url});
+            const result=  await this.app.mysql.insert('uploadfilePath', { url:url,createTime:Date.now()});
             return result     
               
           }
